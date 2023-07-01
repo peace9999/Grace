1cc54cf5914a44dfb13c2609d1027c036714fcc4
peace9999 <138298069+peace9999@users.noreply.github.com>
2023-07-01 13:53:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['27747290834', 'TROKER-TECH', true],
-  ['6281255369012', 'TROKER'S COLLABORATER', true], 
+  ['6281255369012', 'TROKER', true], 
   [''] 
 ] //Number of owners
 
